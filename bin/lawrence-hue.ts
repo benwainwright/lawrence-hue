210b055dd910b7dbb66bc9d9afed6003b02bc689
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-14 23:06:18 +0100

    Stuff

--- bin/lawrence-hue.ts
+++ bin/lawrence-hue.ts
@@ -4,4 +4,9 @@ import * as cdk from "@aws-cdk/core";
 import { LawrenceHueStack } from "../lib/stacks/lawrence-hue-stack";
 
 const app = new cdk.App();
-new LawrenceHueStack(app);
+new LawrenceHueStack(app, {
+  env: {
+    region: "eu-west-2",
+    account: "661272765443"
+  },
+});
--- lib/stacks/lawrence-hue-stack.ts
+++ lib/stacks/lawrence-hue-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as path from "path";
 
 const ID_BASE = "LawrenceHueApp";
@@ -8,12 +9,14 @@ export class LawrenceHueStack extends cdk.Stack {
   constructor(scope: cdk.Construct, props?: cdk.StackProps) {
     super(scope, `${ID_BASE}Stack`, props);
 
-    const function = new lambda.Function(this, `${ID_BASE}AuthFunction`, {
+    const authFunction = new lambda.Function(this, `${ID_BASE}AuthFunction`, {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, "..", "auth"))
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "..", "dist", "lib", "auth"))
     })
 
-
+    new apigateway.LambdaRestApi(this, `${ID_BASE}Api`, {
+      handler: authFunction
+    })
   }
 }
