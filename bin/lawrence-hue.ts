112ed50b8dc4fb7da3be1d6b14815aac970481c2
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-16 09:10:38 +0100

    Stuff

--- bin/lawrence-hue.ts
+++ bin/lawrence-hue.ts
@@ -6,7 +6,7 @@ import { LawrenceHueStack } from "../lib/stacks/lawrence-hue-stack";
 const app = new cdk.App();
 new LawrenceHueStack(app, {
   env: {
-    region: "eu-west-2",
+    region: "us-east-1",
     account: "661272765443"
   },
 });
