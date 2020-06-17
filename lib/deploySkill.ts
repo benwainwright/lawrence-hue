9a8b0a58d51e5b4783a1450f1e9794b78e208ca3
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-17 18:56:34 +0100

    Finished deploy skill command

--- lib/deploySkill.ts
+++ lib/deploySkill.ts
@@ -1,21 +1,14 @@
 import * as AWS from "aws-sdk";
-import { promises as fs } from "fs";
-import * as path from "path";
 import * as execa from "execa";
-
-const LINKING_CONFIG_FILE_NAME = "accountLinking.json";
-
-const dirExists = async (path: string) => {
-  try {
-    const stat = await fs.stat(path);
-    return stat.isDirectory();
-  } catch {
-    return false;
-  }
-};
 
 (async () => {
   const secretsManager = new AWS.SecretsManager({ region: "us-east-1" });
+
+  console.log("Deploying skill");
+  const { stdout } = await execa.command("ask deploy");
+  const skillIdRegex = /^Skill ID: (?<skillId>.*)$/gm;
+  const matches = skillIdRegex.exec(stdout.toString());
+  const skillId = matches?.groups?.skillId;
 
   console.log("Getting Client ID");
   const id = await secretsManager
@@ -27,7 +20,7 @@ const dirExists = async (path: string) => {
     .getSecretValue({ SecretId: "LawrenceHueApp/CLIENT_SECRET" })
     .promise();
 
-  const config = JSON.stringify({
+  const linkingConfig = JSON.stringify({
     accessTokenScheme: "REQUEST_BODY_CREDENTIALS",
     accessTokenUrl: "https://api.meethue.com/oauth2/auth",
     type: "AUTH_CODE",
@@ -35,22 +28,12 @@ const dirExists = async (path: string) => {
     clientSecret: secret.SecretString
   });
 
-  console.log("Writing config to disk");
-
-  if (!(await dirExists("dist"))) {
-    await fs.mkdir("dist");
-  }
-
-  await fs.writeFile(
-    path.resolve(process.cwd(), "dist", LINKING_CONFIG_FILE_NAME),
-    config
-  );
-
-  console.log("Deploying skill");
-  const { stdout } = await execa.command("ask deploy");
-  const skillIdRegex = /^Skill ID: (?<skillId>.*)$/gm;
-  const matches = skillIdRegex.exec(stdout.toString());
-  const skillId = matches?.groups?.skillId;
-
   console.log("Updating account linking info");
+  const command = `ask smapi update-account-linking-info \
+                             --skill-id ${skillId} \
+                             --stage development \
+                             --account-linking-request '${linkingConfig}'`;
+
+  const { stdout: linkingOutput } = await execa.command(command);
+  console.log(linkingOutput);
 })().catch(error => console.log(error));
